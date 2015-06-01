c13045efb8c8befed8c3782a8a0c60036a15f925
Mukul <mukulshm@gmail.com>
2015-06-01 17:59:43 -0500

    Update

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -1,4 +1,6 @@
 class Logo < ActiveRecord::Base
   belongs_to :team
   belongs_to :platform
+  validates :path, :presence => true
+  validates :team_id, :presence => true, :uniqueness => true
 end
--- app/models/path.rb
+++ app/models/path.rb
@@ -1,4 +1,7 @@
 class Path < ActiveRecord::Base
   belongs_to :team
   belongs_to :platform
+  validates :path, :presence => true
+  validates :platform_id, :presence => true
+  validates :team_id, :presence => true
 end
